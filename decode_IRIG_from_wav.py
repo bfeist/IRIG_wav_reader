06f94288ecea8c937692b2df48c876c01ae9142e
Ben Feist <bf@benfeist.com>
2018-04-16 01:15:36 -0400

    Several files processed. Need to add parameters for tape etc

--- decode_IRIG_from_wav.py
+++ decode_IRIG_from_wav.py
@@ -20,7 +20,11 @@ def get_int_by_binary(bits):
     csum = 0
     while len(bits) > 0:
         bits, cbit = bits[:-1], bits[-1]
-        csum += int(cbit) * (pVal[len(bits)])
+        if len(bits) <= len(pVal):
+            csum += int(cbit) * (pVal[len(bits)])
+        else:
+            csum = 0
+            break
     return csum
 
 
@@ -34,9 +38,18 @@ tYears = 0
 blocksize = 8  # 8 = 1ms
 overlap = 0
 
-#filename = 'irig_frame_sample.wav'
-#filename = 'irig_multiframe_sample.wav'
-filename = 'irig_sample_10m.wav'
+# filename = 'irig_frame_sample.wav'
+# filename = 'irig_multiframe_sample.wav'
+# filename = 'irig_sample_10m.wav'
+# filename = 'E:/Apollo_11_Data_Delivery/concatenated_wav_files/T870/defluttered_linear_A11_T870_HR2L_CH31.wav'
+# filename = 'E:/Apollo_11_Data_Delivery/concatenated_wav_files/T869/A11_T869_HR1U_CH1.wav'
+filename = 'E:/Apollo_11_Data_Delivery/concatenated_wav_files/T869/defluttered_linear_A11_T869_HR1U_CH1.wav'
+
+output_file_name_and_path = "T869_defluttered_irig.csv"
+outputFile = open(output_file_name_and_path, "w")
+outputFile.write('file_seconds|file_seconds_diff|IRIG_time|IRIG_in_seconds|irig_seconds_diff|file_to_irig_seconds_diff\n')
+outputFile.close()
+
 rms_threshold = 0.2
 signal_ms_duration = 0
 block_count = 0
@@ -45,8 +58,9 @@ bits = ''
 frame_segment_started = True
 frame_segments = []
 frame_start_time_secs = 0
-
-# print(get_int_by_binary('11100101'))
+decoded_time_count = 0
+first_seconds_decoded = 0
+first_seconds_file = 0
 
 for block in sf.blocks(filename, blocksize=blocksize, overlap=overlap):
     block_count += 1
@@ -60,17 +74,35 @@ for block in sf.blocks(filename, blocksize=blocksize, overlap=overlap):
         if bit == "R":
             if last_bit == "R":  # double R - start new frame
                 if len(frame_segments) == 10:  # If previous frame is not malformed, decode it
-                    tSeconds = get_int_by_binary(frame_segments[0])
-                    tMinutes = get_int_by_binary(frame_segments[1])
-                    tHours = get_int_by_binary(frame_segments[2])
-                    tDays = get_int_by_binary(frame_segments[3])
-                    # Add the additional day bits in segment 4
-                    tDays = tDays + int(frame_segments[4][0]) * pVal[10] + int(frame_segments[4][1]) * pVal[11]
-                    tYears = get_int_by_binary(frame_segments[5])
-                    ctrl_funcs = frame_segments[6] + frame_segments[7] #  unused part of IRIG spec
-                    binary_tod = frame_segments[8] + frame_segments[9]
-                    print('Seconds since file start: ' + "%.2f" % frame_start_time_secs, end='')
-                    print(' | IRIG time: ' + str(tDays).zfill(3) + ':' + str(tHours).zfill(2) + ':' + str(tMinutes).zfill(2) + ':' + str(tSeconds).zfill(2) + ' | in seconds: ' + str(tHours*60*60 + tMinutes*60 + tSeconds))
+                    if decoded_time_count % 10 == 0:
+                        outputFile = open(output_file_name_and_path, "a")
+
+                        tSeconds = get_int_by_binary(frame_segments[0])
+                        tMinutes = get_int_by_binary(frame_segments[1])
+                        tHours = get_int_by_binary(frame_segments[2])
+                        tDays = get_int_by_binary(frame_segments[3])
+                        # Add the additional day bits in segment 4
+                        tDays = tDays + int(frame_segments[4][0]) * pVal[10] + int(frame_segments[4][1]) * pVal[11]
+                        tYears = get_int_by_binary(frame_segments[5])
+                        ctrl_funcs = frame_segments[6] + frame_segments[7] #  unused part of IRIG spec
+                        binary_tod = frame_segments[8] + frame_segments[9]
+                        irig_in_seconds = tHours * 60 * 60 + tMinutes * 60 + tSeconds
+                        if first_seconds_decoded == 0:
+                            first_seconds_decoded = irig_in_seconds
+                            first_seconds_file = frame_start_time_secs
+
+                        irig_seconds_differential = irig_in_seconds - first_seconds_decoded
+                        file_seconds_differential = frame_start_time_secs - first_seconds_file
+                        file_to_irig_seconds_diff = file_seconds_differential - irig_seconds_differential
+                        output_string = 'Seconds since file start: %.3f' % frame_start_time_secs
+                        output_string = output_string + ' | {0:.3f}'.format(file_seconds_differential)
+                        output_string = output_string + ' | IRIG time: {0}:{1}:{2}:{3} | in seconds: {4} | {5} | {6}'.format(str(tDays).zfill(3), str(tHours).zfill(2), str(tMinutes).zfill(2), str(tSeconds).zfill(2), irig_in_seconds, irig_seconds_differential, file_to_irig_seconds_diff)
+                        print(output_string)
+
+                        outputLine = '{0}|{1:.3f}|{2}|{3}|{4}|{5}\n'.format(frame_start_time_secs, file_seconds_differential, str(tDays).zfill(3) + ':' + str(tHours).zfill(2) + ':' + str(tMinutes).zfill(2) + ':' + str(tSeconds).zfill(2), irig_in_seconds, irig_seconds_differential, file_to_irig_seconds_diff)
+                        outputFile.write(outputLine)
+                        outputFile.close()
+                    decoded_time_count += 1
                 # else:
                 #     print(' | IRIG time: malformed')
 
@@ -97,5 +129,6 @@ for block in sf.blocks(filename, blocksize=blocksize, overlap=overlap):
         if signal_ms_duration > 0:
             signal_ms_duration = 0
 
-    # if block_count >= 500000:
+    # if block_count >= 100000:
     #     break
+
